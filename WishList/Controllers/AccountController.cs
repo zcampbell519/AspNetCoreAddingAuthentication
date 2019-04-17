ca82eb4b5132e8f9658e5ef680596c94b5a1521a
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 16:09:12 -0400

    Task 1 Create AccountController Complete

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+
+    }
+}
