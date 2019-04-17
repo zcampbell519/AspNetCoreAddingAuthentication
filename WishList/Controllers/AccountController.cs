cdb06eaa1eed2ff8b0164a84532c7abf1a647225
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 16:34:38 -0400

    Task 7 Check CreateAsync Result HttpPost Register Action Complete

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -34,6 +34,15 @@ namespace WishList.Controllers
 
             var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
 
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", error.Description);
+                }
+                return View(model);
+            }
+
             return RedirectToAction("Index", "Home");
         }
     }
