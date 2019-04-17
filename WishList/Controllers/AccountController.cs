c3ba4b15320255eec03e6eb7c8412870c0040ab6
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 16:26:25 -0400

    Task 3 Create HttpGet Register Action Complete

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -16,5 +16,12 @@ namespace WishList.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
