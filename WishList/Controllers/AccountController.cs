52a235d4449d836f13cdd208872a8174436cc94e
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 16:12:14 -0400

    Task 3 Create Constructor For AccountController Complete

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -10,5 +10,11 @@ namespace WishList.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
     }
 }
