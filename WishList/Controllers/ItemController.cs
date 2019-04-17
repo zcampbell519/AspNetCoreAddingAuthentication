c6415993785477d361e7560481c81805ca0b0e41
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 19:36:41 -0400

    Task 6 Update ItemController.Delete Action Complete

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -45,7 +45,10 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int id)
         {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
             var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            if (item.User.Id != user.Id)
+                return Unauthorized();
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
