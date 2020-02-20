89e2a32e759e280fd9c582a82aa0412549a2d514
Zac <zcampbell@outlook.com>
2020-02-20 15:03:18 -0700

    Section 4.6

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -6,7 +6,7 @@ namespace WishList.Models.AccountViewModels
     {
         [EmailAddress, Required]
         public string Email { get; set; }
-        [Required, StringLength(100),MinLength(8), DataType(DataType.Password)]
+        [Required, StringLength(100, MinimumLength=8), DataType(DataType.Password)]
         public string Password{get;set;}
         [Required,DataType(DataType.Password),Compare("Password")]
         public string ConfirmPassword { get; set; }
