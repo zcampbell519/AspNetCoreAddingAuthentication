7a884fc7b7c4bf603643b98a5f6752e6f96ce470
Zac <zcampbell@outlook.com>
2020-02-20 14:12:22 -0700

    Section 3

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager){
+            this._signInManager=signInManager;
+            this._userManager=userManager;
+        }
+    }
+}
