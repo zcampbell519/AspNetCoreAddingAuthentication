63154f333beffef58c2f880d9dd10860f4c83c3c
Zac <zcampbell@outlook.com>
2020-02-20 15:16:10 -0700

    Section 5

---
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required,DataType(DataType.Password)]
+        public string Password {get;set;}
+    }
+}
