c3dcca2279e7c0fd6efcd258d8e9a1e36d2e753f
Zac <zcampbell@outlook.com>
2020-02-20 14:13:19 -0700

    Section 3.1

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -8,8 +8,8 @@ namespace WishList.Controllers
     [Authorize]
     public class AccountController : Controller
     {
-        private UserManager<ApplicationUser> _userManager;
-        private SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager){
             this._signInManager=signInManager;
